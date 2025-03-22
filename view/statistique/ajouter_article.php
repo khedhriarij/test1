a245679aa3c68319a110380f085ebd641e3126b3
khedhriarij <khedhriarij31@gmail.com>
2025-03-22 05:15:53 +0100

    ajouter_article.php

--- view/statistique/ajouter_article.php
+++ view/statistique/ajouter_article.php
@@ -1,26 +1,26 @@
 <?php require_once "header_admin.php";
 require __DIR__ . '/../../configuration/config.php';
- require_once('../../controller/fonctions_new.php');  ?>
+require_once('../../controller/fonctions_new.php');
+?>
 <link rel="stylesheet" type="text/css" href="login.css">
 <link rel="stylesheet" type="text/css" href="article.css">
-
+<?php 
+if(isset($_POST['ajouter_article'])){
+    enregistrer_nouveau_article();
+}
+?>
 
 
 <body id="page-top">
-
     <!-- Page Wrapper -->
     <div id="wrapper">
-
         <!-- Sidebar -->
         <?php require_once "sidebar_admin.php"; ?>
         <!-- End of Sidebar -->
-
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
             <!-- Main Content -->
             <div id="content">
-
                 <!-- Topbar -->
                 <?php require_once "topbar_admin.php"; ?>
                 <!-- End of Topbar -->
@@ -32,27 +32,35 @@ require __DIR__ . '/../../configuration/config.php';
 <div class="col-lg-15">
 
         <div class="title"><h3>Nouveau article</h3></div>
-        <form action="ajouter_article.php" method="POST" enctype="multipart/form-data">
+        <form action="ajouter_article.php" method="post" enctype="multipart/form-data">
+      
         <div class="form-group">
-            <label for="nom">titre</label>
-            <input type="text" id="nom" name="titre_article" >
+            <label for="inputTitre">titre</label>
+            <input type="text" id="inputTitre" name="titre_article" >
         </div>
         <div class="form-group">
-            <label for="prenom">mots cles</label>
-            <input type="text" id="prenom" name="mots_cles_article" >
+            <label for="inputTags">mots cles</label>
+            <input type="text" id="inputTags" name="mots_cles_article" >
         </div>
-        <div class="form_group">
-        <label>contenu de l'article</label>
-            <textarea name="contenu_article" rows="8" cols="40" ></textarea>
-            
+        <div class="form-group">
+        <label for="summernote">contenu de l'article</label>
+            <textarea name="contenu_article" id="summernote" rows="8" cols="40" 
+             class="form-control"></textarea>
+             <script>
+      $('#summernote').summernote({
+        placeholder: 'Hello Bootstrap 4',
+        tabsize: 2,
+        height: 100
+      });
+    </script>
         </div>
 
         <br>
-        <label >categorie</label><br>
-        <select name="nom_categorie_article" id="">
+        <label for="categorie">categorie</label><br>
+        <select name="nom_categorie_article" id="categorie" class=form-control>
             <option value="">Choisir une categorie </option>
             <?php
-            global $bdd;
+            
     $requete="SELECT * FROM club_essect.categorie ORDER BY id_categorie ASC";
     $result=$bdd->query($requete);
 
@@ -64,7 +72,7 @@ require __DIR__ . '/../../configuration/config.php';
         while($ligne=$result->fetch(PDO::FETCH_ASSOC)){
             $nom_categorie=$ligne['nom_categorie'];
            
-            echo "<option>.$nom_categorie</option>";
+            echo "<option>$nom_categorie</option>";
         }
     }
     ?>
@@ -75,19 +83,27 @@ require __DIR__ . '/../../configuration/config.php';
 
 
 
-        </select>
+        
 <br><br><br>
         <div class="form-group">
-        <label for="photo">photo de profil</label>
-            <input type="file" id="photo" name="image_article" >
+        <label for="image_article">Image de l'article</label>
+            <input type="file" id="image_article" name="image_article" >
             
         </div>
 
 
         <div>
-            <input type="submit" name="ajouter_article" value="soumettre">
+            
+            
+        <input type="submit" name="ajouter_article" value="Soumettre">
+
+
+
         </div>
+        
     </form>
+
+
     </div>
     </div>
     <br><br><br>
@@ -125,4 +141,4 @@ require __DIR__ . '/../../configuration/config.php';
 
 </body>
 
-</html>>
+</html>
